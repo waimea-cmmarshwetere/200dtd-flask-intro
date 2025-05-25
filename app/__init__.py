253cb4cde6db54f21f4c19e91d7888fc6f847648
waimea-cmmarshwetere <cmmarshwetere@waimea.school.nz>
2025-05-26 10:58:00 +1200

    App

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,38 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask import redirect
 from random import randint
 
 # Create the app
 app = Flask(__name__)
 
-
+#------------------------------------------------------
+# Create the app
 @app.get("/")
 def home():
     return render_template('pages/home.jinja')
 
+#------------------------------------------------------
+# Home page - loading a static page
 @app.get("/about/")
 def about():
     return render_template('pages/about.jinja')
 
+#------------------------------------------------------
+# Random Number Page - passing a value into template
 @app.get("/random/")
 def random():
     randNum = randint(1, 1000000000000000000000000000000000000000000000000000000000000000)
     return render_template('pages/random.jinja', number=randNum)
-
+#----------------------------------------------------------------------------------------
+# Number Page - We are getting a value from the route and passing it into the template
 @app.get("/number/<int:num>")
 def analyseNumber(num):
     print(f"You entered: {num}")
     return render_template('pages/number.jinja', number=num)
 
+# Form page - Static page with a form
 @app.get("/form/")
 def form():
     return render_template("pages/form.jinja")
@@ -32,6 +40,19 @@ def form():
 
 
 
+#------------------------------------------------------
+# Handle data  posted from the form
+@app.post("/processForm")
+def processForm():
+    print(f"Form data: ${request.form}")
+    return render_template(
+        "pages/formData.jinja",
+        name = request.form["name"],
+        age = request.form["age"]
+        )
+
+#------------------------------------------------------
+# Handing any missing pages
 @app.errorhandler(404)
 def notFound(e):
     return render_template("pages/404.jinja")
